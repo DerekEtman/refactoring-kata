3cdb85d0b7467d7de09801e549a12589b6bcc114
Derek Etman <dereketman@gmail.com>
2020-05-13 09:26:41 -0700

    refactor: getContents method

--- src/main/java/com/odde/OrdersWriter.java
+++ src/main/java/com/odde/OrdersWriter.java
@@ -9,38 +9,21 @@ public class OrdersWriter {
 
     public String getContents() {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
+        sb.append(getOrderContents());
+
+        if (orders.getOrdersCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        return sb.append("]}").toString();
+    }
+
+    private String getOrderContents(){
+        StringBuffer sb = new StringBuffer();
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(getColorFor(product));
-                sb.append("\", ");
-
-                if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
-                    sb.append("\"size\": \"");
-                    sb.append(getSizeFor(product));
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
-            }
+            sb.append("{\"id\": " + order.getOrderId() + ", \"products\": [");
+            sb.append(getProductsContents(order));
 
             if (order.getProductsCount() > 0) {
                 sb.delete(sb.length() - 2, sb.length());
@@ -49,12 +32,40 @@ public class OrdersWriter {
             sb.append("]");
             sb.append("}, ");
         }
+        return sb.toString();
+    }
 
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
+    private  String getProductsContents(Order order) {
+        StringBuffer sb = new StringBuffer();
+        for (int j = 0; j < order.getProductsCount(); j++) {
+            Product product = order.getProduct(j);
+            sb.append(getProductContents(product));
+        }
+        return sb.toString();
+    }
+
+    private String getProductContents(Product product){
+       StringBuffer sb = new StringBuffer("{");
+        sb.append("\"code\": \"");
+        sb.append(product.getCode());
+        sb.append("\", ");
+        sb.append("\"color\": \"");
+        sb.append(getColorFor(product));
+        sb.append("\", ");
+
+        if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
+            sb.append("\"size\": \"");
+            sb.append(getSizeFor(product));
+            sb.append("\", ");
         }
 
-        return sb.append("]}").toString();
+        sb.append("\"price\": ");
+        sb.append(product.getPrice());
+        sb.append(", ");
+        sb.append("\"currency\": \"");
+        sb.append(product.getCurrency());
+        sb.append("\"}, ");
+        return sb.toString();
     }
 
     private String getSizeFor(Product product) {
